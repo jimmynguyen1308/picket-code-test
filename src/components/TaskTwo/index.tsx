5afc86dd8e6334f69707ab61706995b058508052
Nhat K. (James) Nguyen <118421449+jimmynguyen1308@users.noreply.github.com>
2024-02-24 13:52:15 +0700

    update task 2 description

--- src/components/TaskTwo/index.tsx
+++ src/components/TaskTwo/index.tsx
@@ -4,6 +4,20 @@ export default function TaskTwo() {
   return (
     <>
       <Requirments />
+      <p className="mb-1 mt-8">
+        You can click{" "}
+        <a
+          href="https://github.com/jimmynguyen1308/picket-code-test/tree/master/src/components/TaskTwo/Function"
+          target="_blank"
+          className="underline text-blue-700"
+        >
+          here
+        </a>{" "}
+        to see the files required for the task.
+      </p>
+      <p className="mb-1">
+        To run the task, type "npm run test" to your terminal.
+      </p>
     </>
   )
 }
